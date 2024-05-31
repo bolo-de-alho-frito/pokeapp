218ed76c985b6ac50af7a39cc6b014af614efe2e
Mateus <mateusc.ramos@outlook.com>
2024-05-30 23:42:47 -0300

    refactor: remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Input } from 'antd';
 
 function App() {
   return (
     <div>
       <h1>Pokeapp</h1>
-      <Input placeholder="Basic usage" />
     </div>
   );
 }
